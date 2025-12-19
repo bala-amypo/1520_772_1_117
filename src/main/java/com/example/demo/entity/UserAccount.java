2850dd678854e1df6b36ab310c7d94d0e5dd5ed2
bala-amypo <bala@amypo.in>
2025-12-19 05:36:41 +0000

    User commit

---
+++ src/main/java/com/example/demo/entity/UserAccount.java
@@ -0,0 +1,100 @@
+package com.example.demo.entity;
+
+import java.time.LocalDateTime;
+
+import javax.management.relation.Role;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+
+
+@Entity
+public class e{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String employeeid;
+    private String username;
+    private String email;
+    private String password;
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Role role;
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Status status;
+    private LocalDateTime createdAt = LocalDateTime.now();
+    public enum Role{
+        ADMIN, USER, AUDITOR
+    }
+    public enum Status{
+        ACTIVE, SUSPENDED
+    }
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public String getEmployeeid() {
+        return employeeid;
+    }
+    public void setEmployeeid(String employeeid) {
+        this.employeeid = employeeid;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public Role getRole() {
+        return role;
+    }
+    public void setRole(Role role) {
+        this.role = role;
+    }
+    public Status getStatus() {
+        return status;
+    }
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+    public e(long id, String employeeid, String username, String email, String password, Role role, Status status,
+            LocalDateTime createdAt) {
+        this.id = id;
+        this.employeeid = employeeid;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+        this.status = status;
+        this.createdAt = createdAt;
+    }
+    public e() {
+    }
+    
+}
