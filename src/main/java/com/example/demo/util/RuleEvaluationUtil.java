be6ca871c28f6596691be8d04ee108c38f6c04f9
bala-amypo <bala@amypo.in>
2025-12-23 08:49:27 +0000

    User commit

---
+++ src/main/java/com/example/demo/util/RuleEvaluationUtil.java
@@ -0,0 +1,18 @@
+package com.example.demo.util;
+
+import java.util.List;
+
+public class RuleEvaluationUtil {
+
+    private RuleEvaluationUtil() {
+
+    }
+
+    public static boolean evaluate(Object rule, Object target) {
+        return true;
+    }
+
+    public static boolean evaluate(List<?> rules, Object target) {
+        return true;
+    }
+}
