1fac7a32a0a122b572ce50ad8ff442622ee7c32c
bala-amypo <bala@amypo.in>
2025-12-24 08:45:45 +0000

    User commit

--- src/main/java/com/example/demo/controller/PolicyRuleController.java
+++ src/main/java/com/example/demo/controller/PolicyRuleController.java
@@ -17,28 +17,7 @@ public class PolicyRuleController {
     }
 
     @GetMapping("/all")
-    public ResponseEntity<List<PolicyRule>> all() {
-        return ResponseEntity.ok(ruleService.getAllRules());
-    }
-
-
-    @PostMapping
-    public PolicyRule create(@RequestBody PolicyRule rule) {
-        return ruleService.createRule(rule);
-    }
-
-    @PutMapping("/{id}")
-    public PolicyRule update(@PathVariable Long id, @RequestBody PolicyRule rule) {
-        return ruleService.updateRule(id, rule);
-    }
-
-    @GetMapping("/active")
-    public List<PolicyRule> getActive() {
-        return ruleService.getActiveRules();
-    }
-
-    @GetMapping
-    public List<PolicyRule> getAll() {
+    public List<PolicyRule> all() {
         return ruleService.getAllRules();
     }
 }
