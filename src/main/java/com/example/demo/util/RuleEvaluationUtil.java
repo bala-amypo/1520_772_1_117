f8e2fce6f52f44322f01239182953d78ee96b568
bala-amypo <bala@amypo.in>
2025-12-24 09:25:27 +0000

    User commit

--- src/main/java/com/example/demo/util/RuleEvaluationUtil.java
+++ src/main/java/com/example/demo/util/RuleEvaluationUtil.java
@@ -1,25 +1,24 @@
 package com.example.demo.util;
 
 import com.example.demo.entity.LoginEvent;
-import com.example.demo.repository.PolicyRuleRepository;
+import com.example.demo.entity.ViolationRecord;
 import com.example.demo.repository.ViolationRecordRepository;
 import org.springframework.stereotype.Component;
 
 @Component
 public class RuleEvaluationUtil {
 
-    private final PolicyRuleRepository policyRuleRepository;
     private final ViolationRecordRepository violationRecordRepository;
 
-    public RuleEvaluationUtil(
-            PolicyRuleRepository policyRuleRepository,
-            ViolationRecordRepository violationRecordRepository
-    ) {
-        this.policyRuleRepository = policyRuleRepository;
+    public RuleEvaluationUtil(ViolationRecordRepository violationRecordRepository) {
         this.violationRecordRepository = violationRecordRepository;
     }
 
     public void evaluateLoginEvent(LoginEvent event) {
-    
+        ViolationRecord record = new ViolationRecord();
+        record.setUserId(event.getUserId());
+        record.setViolationType("LOGIN_VIOLATION");
+
+        violationRecordRepository.save(record);
     }
 }
