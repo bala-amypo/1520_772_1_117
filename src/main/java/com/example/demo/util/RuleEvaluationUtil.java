575772a891e6b9da703c62374ca9c9430c9803ef
bala-amypo <bala@amypo.in>
2025-12-24 09:26:47 +0000

    User commit

--- src/main/java/com/example/demo/util/RuleEvaluationUtil.java
+++ src/main/java/com/example/demo/util/RuleEvaluationUtil.java
@@ -2,15 +2,26 @@ package com.example.demo.util;
 
 import com.example.demo.entity.LoginEvent;
 import com.example.demo.entity.ViolationRecord;
+import com.example.demo.repository.PolicyRuleRepository;
 import com.example.demo.repository.ViolationRecordRepository;
 import org.springframework.stereotype.Component;
 
 @Component
 public class RuleEvaluationUtil {
 
+    private final PolicyRuleRepository policyRuleRepository;
     private final ViolationRecordRepository violationRecordRepository;
 
     public RuleEvaluationUtil(ViolationRecordRepository violationRecordRepository) {
+        this.policyRuleRepository = null;
+        this.violationRecordRepository = violationRecordRepository;
+    }
+
+    public RuleEvaluationUtil(
+            PolicyRuleRepository policyRuleRepository,
+            ViolationRecordRepository violationRecordRepository
+    ) {
+        this.policyRuleRepository = policyRuleRepository;
         this.violationRecordRepository = violationRecordRepository;
     }
 
