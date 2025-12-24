571165f07aab257e6ca31b2fe65327c54dab0f17
bala-amypo <bala@amypo.in>
2025-12-24 09:08:36 +0000

    User commit

--- src/main/java/com/example/demo/util/RuleEvaluationUtil.java
+++ src/main/java/com/example/demo/util/RuleEvaluationUtil.java
@@ -8,14 +8,20 @@ import org.springframework.stereotype.Component;
 @Component
 public class RuleEvaluationUtil {
 
-    public RuleEvaluationUtil() {
-    }
+    private final ViolationRecordRepository violationRecordRepository;
 
-    public RuleEvaluationUtil(PolicyRuleRepository policyRuleRepository,
-                              ViolationRecordRepository violationRecordRepository) {
+    public RuleEvaluationUtil(ViolationRecordRepository violationRecordRepository) {
+        this.violationRecordRepository = violationRecordRepository;
     }
 
     public void evaluateLoginEvent(LoginEvent event) {
 
+        ViolationRecord violation = new ViolationRecord();
+        violation.setUserId(event.getUserId());
+        violation.setViolationType("LOGIN_POLICY_VIOLATION");
+        violation.setSeverity("HIGH");
+        violation.setResolved(false);
+
+        violationRecordRepository.save(violation);
     }
 }
