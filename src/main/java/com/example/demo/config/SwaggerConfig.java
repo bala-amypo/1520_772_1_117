6b47c622c70d549b9234de4d2e73f32f0c0819fb
bala-amypo <bala@amypo.in>
2025-12-26 09:54:34 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -1,9 +1,12 @@
 package com.example.demo.config;
-
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 import java.util.List;
 
 @Configuration
@@ -11,9 +14,21 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
+
+        SecurityScheme bearerScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT");
+
         return new OpenAPI()
                 .servers(List.of(
-                        new Server().url("https://9044.pro604cr.amypo.ai/") // This is where your Swagger UI server is hosted
-                ));
+                        new Server().url("https://9063.pro604cr.amypo.ai/")
+                ))
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth", bearerScheme)
+                )
+                .addSecurityItem(
+                        new SecurityRequirement().addList("bearerAuth")
+                );
     }
 }
